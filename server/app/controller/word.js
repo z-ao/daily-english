6c95ea420dcadc399797b080b434c0f06fd9a6df
Lea <452224689@qq.com>
2019-07-13 20:47:47 +0800

    update: audio origin

--- server/app/controller/word.js
+++ server/app/controller/word.js
@@ -11,7 +11,7 @@ class WordController extends Controller {
     const wordRandom = await ctx.service.word.random(count);
 
     //设置单词录音
-    const { host } = ctx;
+    const { host } = ctx.header;
     const audioURL = (word) => `https://${host}/public/word/${word.slice(0, 1)}/${word}.mp3`;
     wordRandom.forEach(item => {
       item.dataValues.audio = audioURL(item.word);
